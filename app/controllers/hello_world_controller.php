66da519d4ad81328245c143824016997d426f03d
juhopaak <juho.paakkonen@helsinki.fi>
2016-04-04 11:45:46 +0300

    Näkymien suunnittelu

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,29 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  //View::make('home.html');
+      echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
+
+    public static function huutokauppa(){
+      View::make('suunnitelmat/etusivu.html');
+    }
+
+    public static function tuote(){
+      View::make('suunnitelmat/tuote.html');
+    }
+
+    public static function muokkaa(){
+      View::make('suunnitelmat/muokkaa_tuotetta.html');
+    }
+
+    public static function kirjautuminen(){
+      View::make('suunnitelmat/kirjautuminen.html');
+    }
+
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,19 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/huutokauppa', function() {
+  	HelloWorldController::huutokauppa();
+  });
+
+  $routes->get('/huutokauppa/1', function() {
+  	HelloWorldController::tuote();
+  });
+
+  $routes->get('/huutokauppa/1/muokkaa', function() {
+  	HelloWorldController::muokkaa();
+  });
+
+  $routes->get('/huutokauppa/kirjautuminen', function() {
+  	HelloWorldController::kirjautuminen();
+  });
